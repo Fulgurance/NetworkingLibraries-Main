7a6adf4327dbd8fb6ab4cbb29b4beb1cc82fddfa
fulgurance <zohran.londais@gmail.com>
2023-04-05 22:47:18 +0100

    Added Nghttp2 library installer

---
+++ Nghttp2/1.44.0/1.44.0.cr
@@ -0,0 +1,25 @@
+class Target < ISM::Software
+    
+    def configure
+        super
+
+        configureSource([   "--prefix=/usr",
+                            "--disable-static",
+                            "--enable-lib-only",
+                            "--docdir=/usr/share/doc/nghttp2-1.44.0"],
+                            buildDirectoryPath)
+    end
+
+    def build
+        super
+
+        makeSource(path: buildDirectoryPath)
+    end
+    
+    def prepareInstallation
+        super
+
+        makeSource(["DESTDIR=#{builtSoftwareDirectoryPath}/#{Ism.settings.rootPath}","install"],buildDirectoryPath)
+    end
+
+end
